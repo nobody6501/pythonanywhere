1658647c1e1752ade159638a4c1edda2e9f129b3
Martin Huang <nobody6501@utexas.edu>
2022-07-12 12:23:33 +0000

    setup app dir and added config

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,14 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
+if __name__ == '__main__':
+    app.run(debug=True)
